cbc678983c06fde873c090b4d8c1436536a36276
anniechakraborty <the.demented.genius29@gmail.com>
2025-05-02 16:58:54 +0200

    extract text

--- trials/extract_paper.py
+++ trials/extract_paper.py
@@ -1,6 +1,6 @@
 import fitz
-from pdf2image import convert_from_path
-import cv2
+# from pdf2image import convert_from_path
+# import cv2
 import numpy as np
 
 # Extract text from PDF
@@ -13,19 +13,7 @@ def extract_text_from_pdf(pdf_path):
     doc.close()
     return text
 
-# Extract images from PDF
-def extract_images_from_pdf(pdf_path):
-    images = convert_from_path(pdf_path)
-    return images  # List of PIL.Image objects
-
-# Process images using OpenCV (example)
-def process_images(images):
-    for img in images:
-        cv_img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        # Process each image as needed
-
-# Example usage
-pdf_path = 'path_to_your_pdf.pdf'
+pdf_path = 'A6-MON~1.pdf'
 text = extract_text_from_pdf(pdf_path)
-images = extract_images_from_pdf(pdf_path)
-process_images(images)
+
+print(text)
